a618e244ae9c229c654bc6566103f43f5701bcb2
torrent benoit <torrentdu64@gmail.com>
2017-05-26 19:41:16 +0200

    test de twinMax.js c'est juste enorme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+(function($){
+  var card = $('.card').height(0);
+  card.mouseover(function(e){
+    card.animate({height:400},400);
+  })
+
+})(jQuery);
