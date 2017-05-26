054f67b63bbadc0f8d5365c9c49aa1639b433f8c
torrent benoit <torrentdu64@gmail.com>
2017-05-26 21:15:39 +0200

    add tweenMax refacto to yeild[:tween]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,17 @@
 //= require_tree .
 
 (function($){
-  var card = $('.card').height(0);
+  var card = $('.card');
+  var name = $('.card-category').css({ opacity: 0 });
+  var text = $('.card-description').css({opacity: 0});
+
+
   card.mouseover(function(e){
-    card.animate({height:400},400);
+    var t1 = new TimelineLite();
+    t1.to(card, 1, {height:400});
+    t1.to(name, 1,{alpha: 1});
+    t1.to(text, 1.2, {alpha: 1});
+    t1.to(text, 1,  {x:150,y:-200})
   })
 
 })(jQuery);
