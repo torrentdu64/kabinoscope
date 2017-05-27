361684952f7ffc1c668fe18eec77b9b1079fa5fd
torrent benoit <torrentdu64@gmail.com>
2017-05-27 14:04:54 +0200

    add content js tween.js and yield[js] to layout  application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require app
 //= require_tree .
 
-(function($){
-  var card = $('.card');
-  var name = $('.card-category').css({ opacity: 0 });
-  var text = $('.card-description').css({opacity: 0});
 
-
-  card.mouseover(function(e){
-    var t1 = new TimelineLite();
-    t1.to(card, 1, {height:400});
-    t1.to(name, 1,{alpha: 1});
-    t1.to(text, 1.2, {alpha: 1});
-    t1.to(text, 1,  {x:150,y:-200})
-  })
-
-})(jQuery);
